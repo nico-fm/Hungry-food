208a42573aa2a0b3e075dc17dde7b1dfdb81f16f
CentralCornet8 <gabasteitor-300@hotmail.com>
2017-03-07 17:56:47 -0700

    viscachas asesinas

--- app/src/main/java/com/example/hp/hungryfood/MainActivity.java
+++ app/src/main/java/com/example/hp/hungryfood/MainActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Aqui desde mi casa les mando mucho flow...Byronth eres una mierda
+    //si se ve..Byronth..voy a hacer una imagen de vos con viscachas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //se ve?   ....
-        //si se ve..Byronth..voy a hacer una imagen de vos con viscachas
+
 
 
 
